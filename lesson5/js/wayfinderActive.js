16d70cdc8c4a859e1b7d29c6a1bd13d4f2c54046
Huffman-Kristopher <nokins_@hotmail.com>
2020-10-14 15:30:19 -0700

    nav wayfinder

--- lesson5/js/wayfinderActive.js
+++ lesson5/js/wayfinderActive.js
@@ -1,5 +1,5 @@
 var header = document.getElementById("nav1");
-var btns = header.getElementsByClassName("nav1Btn);
+var btns = header.getElementsByClassName("nav1Btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
