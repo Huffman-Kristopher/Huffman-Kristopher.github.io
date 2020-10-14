32477c8f0537779a9548e427e5ff2f4eca551f51
Huffman-Kristopher <nokins_@hotmail.com>
2020-10-14 11:31:49 -0700

    work on lesson 5

---
+++ lesson5/js/wayfinderActive.js
@@ -0,0 +1,9 @@
+var header = document.getElementById("nav1");
+var btns = header.getElementsByClassName("nav1Btn);
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
