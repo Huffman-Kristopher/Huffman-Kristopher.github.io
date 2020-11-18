b2e74006f30b9411004381744dc4ae8b5daee2f8
Huffman-Kristopher <nokins_@hotmail.com>
2020-11-18 12:26:39 -0800

    work on week 10 assignment

---
+++ lesson10/js/weathersummary.js
@@ -0,0 +1,19 @@
+const weatherSummary = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=11fee94c13af8bc2e5449df290a1fcee";
+fetch(weatherSummary)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+
+        const condition = document.querySelector('#condition');
+        const temp = document.querySelector('#temp');
+        const high = document.querySelector('#high');
+        const humidity = document.querySelector('#humidity');
+        const speed = document.querySelector('#speed');
+
+        condition.textContent = `${jsObject.weather[0].main}`;
+        temp.textContent = `${jsObject.main.temp} °F`;
+        high.textContent = `${jsObject.main.temp_max} °F`;
+        humidity.textContent = `${jsObject.main.humidity}%`;
+        speed.textContent = `${jsObject.wind.speed} mph`;
+
+    });
